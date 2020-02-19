4509d595202700bcfee142aab93162747f4ea016
muli3203 <muli3203@gmail.com>
2020-02-19 14:36:46 +0300

    add flask extensions

--- app/__init__.py
+++ app/__init__.py
@@ -1,36 +1,44 @@
 from flask import Flask
-from config import config_options
-from flask_migrate import Migrate
+from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_uploads import UploadSet, configure_uploads, IMAGES
 
-login_manager = LoginManager()
-login_manager.session_protection = 'strong'
-login_manager.login_view = 'auth.login'
+from config import config_options
 
-db = SQLAlchemy()
-migrate = Migrate()
-# login_manager = LoginManager()
-
-# login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
+db = SQLAlchemy()
+migrate = Migrate()
+login_manager = LoginManager()
+login_manager.session_protection = "strong"
+login_manager.login_view = "auth.login"
 
-# Let's rewrite this to be configurable from a Factory
-def create_app(config_name):
+photos = UploadSet("photos", IMAGES)
+mail = Mail()
+
+
+def create_app(config_name="development"):
     app = Flask(__name__)
+
+    # Creating the app configurations
     app.config.from_object(config_options[config_name])
-    from .auth import auth as authentication_blueprint
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    # Registering the blueprint
     from .main import main as main_blueprint
 
-    app.register_blueprint(authentication_blueprint)
     app.register_blueprint(main_blueprint)
+    from .auth import auth as auth_blueprint
 
-    login_manager.init_app(app)
-    db.init_app(app)
-    bootstrap.init_app(app)
-
+    app.register_blueprint(auth_blueprint, url_prefix="/authenticate")
 
     return app
-    
